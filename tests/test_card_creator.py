ff0bf0164b0934b59373b2a9dde25839f20c4a42
Kuroflectr <ike.kuroflectr@gmail.com>
2024-05-02 15:04:09 +0900

    fixed lint

--- tests/test_card_creator.py
+++ tests/test_card_creator.py
@@ -1,6 +1,6 @@
+import os
 from pathlib import Path
 from typing import Dict
-import os
 
 import pytest
 
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,2 @@
-from pathlib import Path
-from src.models import AnkiNoteResponse
-import pytest
-
-
 def test_create_message():
     pass
